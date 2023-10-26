43fa67ded77d43fbbd457efbfe2e10b2c7e03967
Yusuf Sancak <yusuf@4alabs.io>
2023-10-26 23:28:19 +0300

    adding colors util

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+export const primaryColor = "#1d2226"
+export const bgColor = "#ebebeb"
+
+//typography
+export const textColor = "#000000"
+export const captionTextColor = "#7D7D7D"
+
+//states
+export const errorColor = "#F44336"
+export const successColor = "#4CAF50"
+export const warningColor = "#FFC107"
+export const infoColor = "#2196F3"
