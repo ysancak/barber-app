6befdd1996ccc92d70ca1c7516a91ccdb4379cb2
Yusuf Sancak <yusufsancakk@gmail.com>
2024-09-09 14:12:17 +0300

    updated

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -78,7 +78,12 @@ export const openUrl = url => {
 };
 
 export const checkAndRequestLocationPermission = async () => {
-  await request(PERMISSIONS.IOS.LOCATION_ALWAYS)
+  const permission =
+    Platform.OS === 'ios'
+      ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+      : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+
+  await request(permission)
     .then(result => {
       switch (result) {
         case 'granted':
